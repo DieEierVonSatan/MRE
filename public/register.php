80f11c1e09285a771ed9be25cf760ae993d174e3
DieEierVonSatan <47187431+DieEierVonSatan@users.noreply.github.com>
2019-11-04 17:23:24 +0100

    Create register.php

---
+++ public/register.php
@@ -0,0 +1,30 @@
+<?php require('../private/initialize.php') ?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>MRE</title>	 
+        <link rel="stylesheet" type="text/css" href="stylesheets/style.css">
+    </head>
+    
+    <body>
+        <header>
+            <h1></h1>
+        </header>
+
+        <nav>
+        <ul>
+            <li><a href="frontpage.php">Home</a></li>
+        </ul>
+        <nav>
+
+        <body>
+        TODO, create register form
+        </body>
+
+        <footer>
+            &copy;<?php echo date('Y');?> MRE
+        </footer>
+    </body>
+</html>
