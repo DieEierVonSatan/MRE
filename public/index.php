112013bc299a6320c9a935a7d3ac80c880fe49c8
DieEierVonSatan <47187431+DieEierVonSatan@users.noreply.github.com>
2019-11-04 17:23:12 +0100

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,213 @@
+<?php require_once('../private/initialize.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>MRE</title>
+	 
+	<link rel="stylesheet" type="text/css" href="stylesheets/style.css">
+	<script src="https://kit.fontawesome.com/9377fb804b.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="main-header">
+<header>
+	<div class="container1">
+
+<h1>Music Recommendation Engine</h1>
+</div>
+
+
+</header>
+
+<nav class="navbar">
+<header>
+	<div class="container1">
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Register</a></li>
+				<li><a href="#">login</a></li>
+				<li><a href="#">Contact</a></li>
+				<li><a href="#">About</a></li>
+                <li><a href="staff/index.php">Staff</a></li>
+			</ul>
+</div>
+
+	<b class="search_box">
+		<input class="search-txt" type="text" size=60 name="" placeholder="Search your favorite music...">
+		<b class="search-btn" herf="">
+	<i class="fas fa-search"></i>
+		</b>
+	</b>
+
+
+</nav>
+
+</div>
+	<div class="main-body">
+		<h1>Recommended music</h1>
+	</div>
+
+	<div class="container">
+	
+		<div class="box">
+		<div class="imgBox">
+			<img src="Images/coolimage5654.jfif" width="225" height="225" >
+					</div>
+					<div class="details">
+					<div class="content">
+						<h1>music name and the musician</h1>
+						<p>details about the music</p><br />
+						<div class="audio">
+		 <audio controls style="width: 100% ;">
+   <source src="audio/a.mp3" type="audio/mpeg">
+</audio>
+						</div>
+					</div>
+				</div>
+		</div>
+		<div class="box"><div class="imgBox">
+			<img src="Images/coolimage5654.jfif" width="225"  height="225" >
+					</div>
+					<div class="details">
+					<div class="content">
+						<h1>music name and the musician</h1>
+						<p>details about the music</p><br />
+						<div class="audio">
+		 <audio controls style="width: 100% ;">
+   <source src="audio/a.mp3" type="audio/mpeg">
+</audio>
+						</div>
+					</div>
+				</div></div>
+		<div class="box"><div class="imgBox">
+			<img src="Images/coolimage5654.jfif" width="225" height="225" >
+					</div>
+					<div class="details">
+					<div class="content">
+						<h1>music name and the musician</h1>
+						<p>details about the music</p><br />
+						<div class="audio">
+		 <audio controls style="width: 100% ;">
+   <source src="audio/a.mp3" type="audio/mpeg">
+</audio>
+						</div>
+					</div>
+				</div></div>
+		<div class="box"><div class="imgBox">
+			<img src="Images/coolimage5654.jfif" width="225" height="225" >
+					</div>
+					<div class="details">
+					<div class="content">
+						<h1>music name and the musician</h1>
+						<p>details about the music</p><br />
+						<div class="audio">
+		 <audio controls style="width: 100% ;">
+   <source src="audio/a.mp3" type="audio/mpeg">
+</audio>
+						</div>
+					</div>
+				</div></div>
+		<div class="box"><div class="imgBox">
+			<img src="Images/coolimage5654.jfif" width="225" height="225" >
+					</div>
+					<div class="details">
+					<div class="content">
+						<h1>music name and the musician</h1>
+						<p>details about the music</p><br />
+						<div class="audio">
+		 <audio controls style="width: 100% ;">
+   <source src="audio/a.mp3" type="audio/mpeg">
+</audio>
+						</div>
+					</div>
+				</div></div>
+		<div class="box"><div class="imgBox">
+			<img src="Images/coolimage5654.jfif" width="225" height="225" >
+					</div>
+					<div class="details">
+					<div class="content">
+						<h1>music name and the musician</h1>
+						<p>details about the music</p><br />
+						<div class="audio">
+		 <audio controls style="width: 100% ;">
+   <source src="audio/a.mp3" type="audio/mpeg">
+</audio>
+						</div>
+					</div>
+				</div></div>
+		<div class="box"><div class="imgBox">
+			<img src="Images/coolimage5654.jfif" width="225" height="225" >
+					</div>
+					<div class="details">
+					<div class="content">
+						<h1>music name and the musician</h1>
+						<p>details about the music</p><br />
+						<div class="audio">
+		 <audio controls style="width: 100% ;">
+   <source src="audio/a.mp3" type="audio/mpeg">
+</audio>
+						</div>
+					</div>
+				</div></div>
+		<div class="box"><div class="imgBox">
+			<img src="Images/coolimage5654.jfif" width="225" height="225" >
+					</div>
+					<div class="details">
+					<div class="content">
+						<h1>music name and the musician</h1>
+						<p>details about the music</p><br />
+						<div class="audio">
+		 <audio controls style="width: 100% ;">
+   <source src="audio/a.mp3" type="audio/mpeg">
+</audio>
+						</div>
+					</div>
+				</div></div>
+		<div class="box"><div class="imgBox">
+			<img src="Images/coolimage5654.jfif" width="225" height="225" >
+					</div>
+					<div class="details">
+					<div class="content">
+						<h1>music name and the musician</h1>
+						<p>details about the music</p><br />
+						<div class="audio">
+		 <audio controls style="width: 100% ;">
+   <source src="audio/a.mp3" type="audio/mpeg">
+</audio>
+						</div>
+					</div>
+				</div></div>
+		<div class="box"><div class="imgBox">
+			<img src="Images/coolimage5654.jfif" width="225" height="225" >
+					</div>
+					<div class="details">
+					<div class="content">
+						<h1>music name and the musician</h1>
+						<p>details about the music</p><br />
+						<div class="audio">
+		 <audio controls style="width: 100% ;">
+   <source src="audio/a.mp3" type="audio/mpeg">
+</audio>
+						</div>
+					</div>
+				</div></div>
+		
+	</div>
+	<div class="spcse">
+	</div>
+</body>
+
+
+	<footer>
+	<div class="footer">
+	<h1>Thank you for visiting our website!</h1>
+	</div>
+	<div class= "contactgegevens">
+	<h1>contact gegevens</h1>
+	<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo,</p>
+	</div>
+	
+	
+	</footer>
+</body>
+</html>
