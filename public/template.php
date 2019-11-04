46bbe0a09c68e4b2ffc961a49c24e91200b13916
DieEierVonSatan <47187431+DieEierVonSatan@users.noreply.github.com>
2019-11-04 17:23:59 +0100

    Create template.php

---
+++ public/template.php
@@ -0,0 +1,24 @@
+<?php require('../../private/initialize.php') ?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>MRE</title>	 
+        <link rel="stylesheet" type="text/css" href="#">
+    </head>
+    
+    <body>
+        <header>
+            <h1></h1>
+        </header>
+
+        <nav>
+        
+        <nav>
+
+        <footer>
+            &copy;<?php echo date('Y');?> MRE
+        </footer>
+    </body>
+</html>
