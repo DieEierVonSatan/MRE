33883f2d9ca4fc557f6046416fd85cd476d21f75
DieEierVonSatan <47187431+DieEierVonSatan@users.noreply.github.com>
2019-11-04 17:23:26 +0100

    Create index.php

---
+++ public/staff/index.php
@@ -0,0 +1,28 @@
+<?php require('../../private/initialize.php') ?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>MRE</title>	 
+        <link rel="stylesheet" type="text/css" href="stylesheets/style.css">
+    </head>
+    
+    <body>
+        <header>
+            <h1></h1>
+        </header>
+
+        <nav>
+        
+        <nav>
+
+        <body>
+            TODO, show admin database
+        </body>
+
+        <footer>
+            &copy;<?php echo date('Y');?> MRE
+        </footer>
+    </body>
+</html>
